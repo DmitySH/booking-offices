4967acc4e1003194fbddd60f8f2e1aef90dc3d57
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2022-05-22 19:06:18 +0300

    feat: add basic admin for models

--- src/general/admin.py
+++ src/general/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from src.general.models import Country, City, RatingStar
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RatingStar)
+class RatingStarAdmin(admin.ModelAdmin):
+    pass
--- src/main/admin.py
+++ src/main/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from src.main.models import Office, Reservation, OfficeReview, OfficePhoto
+
+
+@admin.register(Office)
+class OfficeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(OfficeReview)
+class OfficeReviewAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(OfficePhoto)
+class OfficePhotoAdmin(admin.ModelAdmin):
+    pass
