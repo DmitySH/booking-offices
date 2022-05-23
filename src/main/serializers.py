09f35d09b4d79b7284adb048bb6cee6e97612543
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2022-05-23 19:27:27 +0300

    feat: add custom city and country fields

--- src/main/serializers.py
+++ src/main/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from src.general.models import Country, City
 from src.main import models
 
 
@@ -8,6 +9,32 @@ class OfficeSerializer(serializers.ModelSerializer):
     Serializes office.
     """
 
+    country = serializers.CharField(source='country.name')
+    city = serializers.CharField(source='city.name')
+
     class Meta:
         model = models.Office
         fields = '__all__'
+
+    def validate(self, attrs):
+        errors = dict()
+        try:
+            attrs['country'] = Country.objects.get(**attrs['country'])
+        except Country.DoesNotExist:
+            errors['country'] = 'No such country'
+
+        try:
+            attrs['city'] = City.objects.get(**attrs['city'])
+        except City.DoesNotExist:
+            errors['city'] = 'No such city'
+
+        if len(errors) > 0:
+            raise serializers.ValidationError(errors)
+
+        return super().validate(attrs)
+
+    def update(self, instance, validated_data):
+        instance.country = validated_data.pop('country')
+        instance.city = validated_data.pop('city')
+
+        return super().update(instance, validated_data)
